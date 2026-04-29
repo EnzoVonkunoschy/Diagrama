62b3371075c0d54a0bce86d1728ea5dedc582aed
Cristina Mendoza <mendozacrsitina234@gmail.com>
2026-04-29 10:34:44 -0300

    Primer commit

--- Tarea.java
+++ Tarea.java
@@ -1,2 +1,49 @@
 public class Tarea {
+
+
+    private int duracion;
+    private String nombre;
+    private String descripcion;
+    private Tarea antecesora;
+    private Tarea sucesora;
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Tarea getAntecesora() {
+        return antecesora;
+    }
+
+    public void setAntecesora(Tarea antecesora) {
+        this.antecesora = antecesora;
+    }
+
+    public Tarea getSucesora() {
+        return sucesora;
+    }
+
+    public void setSucesora(Tarea sucesora) {
+        this.sucesora = sucesora;
+    }
 }
