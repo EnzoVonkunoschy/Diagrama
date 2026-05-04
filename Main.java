6ce487f1611a7d6be858b3babd4c8176b82094f6
Cristina Mendoza <mendozacrsitina234@gmail.com>
2026-05-04 16:56:10 -0300

    Agrego menu y manejo de tareas

--- Main.java
+++ Main.java
@@ -1,15 +1,69 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Tarea> lista = new ArrayList<>();
+
+        int opcion = 0;
+
+        while (opcion != 4) {
+
+            System.out.println("\n1- Crear tarea");
+            System.out.println("2- Mostrar tareas");
+            System.out.println("3- Enlazar tareas");
+            System.out.println("4- Salir");
+            System.out.print("Elegir: ");
+
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            if (opcion == 1) {
+
+                System.out.print("Nombre: ");
+                String nom = sc.nextLine();
+
+                System.out.print("Duracion: ");
+                int dur = sc.nextInt();
+                sc.nextLine();
+
+                System.out.print("Descripcion: ");
+                String desc = sc.nextLine();
+
+                Tarea t = new Tarea(dur, nom, desc);
+                lista.add(t);
+
+                System.out.println("Tarea agregada");
+
+            } else if (opcion == 2) {
+
+                for (int i = 0; i < lista.size(); i++) {
+                    System.out.println(i + ": " + lista.get(i).mostrar());
+                }
+
+            } else if (opcion == 3) {
+
+                if (lista.size() < 2) {
+                    System.out.println("Faltan tareas");
+                } else {
+
+                    System.out.print("Indice tarea anterior: ");
+                    int a = sc.nextInt();
+
+                    System.out.print("Indice tarea siguiente: ");
+                    int b = sc.nextInt();
+
+                    lista.get(a).setSucesora(lista.get(b));
+                    lista.get(b).setAntecesora(lista.get(a));
+
+                    System.out.println("Enlazadas");
+                }
+            }
         }
+
+        sc.close();
     }
 }
